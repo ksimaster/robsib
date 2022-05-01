448e8f7be302d9b6b7dba41aeb8d878c968d4b27
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2022-05-01 12:37:28 +0300

    залиты проекты
    1

---
+++ Assets/ImportPack/Drovogrel/Assets/Scripts/DestroyHome.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHome : MonoBehaviour
+{
+   public GameObject [] homeParts = new GameObject[50];
+
+    void Start()
+    {
+
+            StartCoroutine("HideHome");
+
+    }
+
+    IEnumerator HideHome()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            homeParts[i].SetActive(false);
+            Debug.Log(i + " элемент уничтожен");
+            yield return new WaitForSeconds(3.0f);
+        }
+        
+        
+    }
+
+
+}
---
+++ Assets/ImportPack/Drovogrel/Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject button_PutInFire, button_Trow, button_Take;
+    public int log;
+    public int max_Log = 5;
+    public string collisionTagLog;
+    public string collisionTagFire;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckLog();
+    }
+
+    void OnOffButton()
+    {
+        
+    }
+
+   public void LogInHands()
+    {
+        if (log < max_Log)
+        {
+            log++;
+            Debug.Log(log);
+        }
+    }
+    public void LogOutHands()
+    {
+        if (log > 0)
+        {
+            log--;
+            Debug.Log(log);
+        }
+    }
+
+    void CheckLog() { 
+        if (log != 0) { 
+            button_Trow.SetActive(true);
+        } else if (log==0)
+        {
+            button_Trow.SetActive(false);
+            button_PutInFire.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag(collisionTagLog))
+        {
+           
+            button_Take.SetActive(true);
+
+        }
+       /* else
+        {
+            button_Take.SetActive(false);
+        }
+*/
+        if (col.gameObject.CompareTag(collisionTagFire) && log!=0)
+        {
+            button_PutInFire.SetActive(true);
+
+        }
+        else
+        {
+            button_PutInFire.SetActive(false);
+        }
+
+    }
+
+ 
+}
+
