4bab899f8e04b724621ba347575ff932869f8d26
ivan.mochalov <ivan.mochalov@aspose.com>
2022-05-29 01:49:05 +0300

    Пример вызова JS

---
+++ Assets/Scripts/Adv.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Adv : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test JS");
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLPluginJS.CallFunction();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
