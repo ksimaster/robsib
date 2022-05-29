22928b8d448cddd72678e449c09868506a448b5b
ivan.mochalov <ivan.mochalov@aspose.com>
2022-05-29 17:53:11 +0300

    fix frost

--- Assets/Scripts/Constants/PlayerConstants.cs
+++ Assets/Scripts/Constants/PlayerConstants.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public static class PlayerConstants
 {
-    public const float SpeedFrost = 0.015f;
+    public const float SpeedFrost = 0.013f;
     public const float FuelSpendIdle = 0.0001f;
 
     public const string Fuel = "Fuel";
     public const float MaxFuel = 100;
-    public const float FuelConsumption = 0.001f;
+    public const float FuelConsumption = 0.0015f;
 
     public const string FuelCar = "FuelCar";
     public const float MaxFuelCar = 10;
