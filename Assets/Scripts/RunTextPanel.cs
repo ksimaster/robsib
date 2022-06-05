e2ec9fd513e0ce941305bc6839996b29428aa97b
ksimaster <36484548+ksimaster@users.noreply.github.com>
2022-06-05 19:11:49 +0300

    Update RunTextPanel.cs

--- Assets/Scripts/RunTextPanel.cs
+++ Assets/Scripts/RunTextPanel.cs
@@ -9,8 +9,14 @@ public class RunTextPanel : MonoBehaviour
     private string textToRun;
     public float speedText;
     int randMessage;
+    private int i = 0;
    // public Text textShutka;
-    public bool isNeedRange;
+    //public bool isNeedRange;
+    public bool [] isNeedRanges = {false, false, false};
+    public float [] temperatures = {-14, -20, -30};
+    
+    public string [][] groupMessages = new string [3][];
+
     string[] messages =  { "Плох тот генерал, который не перестал быть солдафоном. ",
     "Военнослужащий возит за собой свою семью по частям. ",
     "Профессор понял, что совершил ошибку, передав секрет своего смертельного оружия военным. Но было уже поздно. И он лег спать.",
@@ -23,26 +29,50 @@ public class RunTextPanel : MonoBehaviour
     private void Awake()
     {
         PanelMessage.SetActive(false);
+        groupMessages [0] = new string[] { "Плох тот генерал, который не перестал быть солдафоном. ",
+        "Военнослужащий возит за собой свою семью по частям. ",
+        "Профессор понял, что совершил ошибку, передав секрет своего смертельного оружия военным. Но было уже поздно. И он лег спать.",
+        "Когда американский солдат идёт в атаку, он не боится ничего, потому что застрахован. Когда русский солдат идёт в атаку, тогда никто не застрахован…",
+        "Армия — дело сугубо добровольное: хочешь — иди, не хочешь — заберут."
+     };
+        groupMessages [1] = new string[] { "Плох тот генерал, который не перестал быть солдафоном. ",
+        "Военнослужащий возит за собой свою семью по частям. ",
+        "Профессор понял, что совершил ошибку, передав секрет своего смертельного оружия военным. Но было уже поздно. И он лег спать.",
+        "Когда американский солдат идёт в атаку, он не боится ничего, потому что застрахован. Когда русский солдат идёт в атаку, тогда никто не застрахован…",
+        "Армия — дело сугубо добровольное: хочешь — иди, не хочешь — заберут."
+     };
+        groupMessages [2] = new string[] { "Плох тот генерал, который не перестал быть солдафоном. ",
+        "Военнослужащий возит за собой свою семью по частям. ",
+        "Профессор понял, что совершил ошибку, передав секрет своего смертельного оружия военным. Но было уже поздно. И он лег спать.",
+        "Когда американский солдат идёт в атаку, он не боится ничего, потому что застрахован. Когда русский солдат идёт в атаку, тогда никто не застрахован…",
+        "Армия — дело сугубо добровольное: хочешь — иди, не хочешь — заберут."
+     };
     }
     private void Start()
     {
         //textToRun = TextGameObject.text;
         textOnPanel.text = "";
-        
+       
     }
     private void FixedUpdate()
     {
-        if (isNeedRange) return;
-        if (sliderOut.value < -13 && sliderOut.value > -14)
+        if(i >= isNeedRanges.Length || i >= groupMessages.Length || i >= temperatures.Length) return;
+        if (isNeedRanges[i]) return;
+        
+        if (sliderOut.value < temperatures[i] + 0.5f && sliderOut.value > temperatures[i] - 0.5f)
         { 
-            isNeedRange = true;
+            textOnPanel.text = "";
+            isNeedRanges[i] = true;
             PanelMessage.SetActive(true);
-            GetMessageText();
+            GetMessageText(groupMessages[i]);
+            i++;
 
             StartCoroutine(TextRunCoroutine());
         }
+        
+        
     }
-    public void GetMessageText()
+    public void GetMessageText(string [] messages)
     {
         randMessage = Random.Range(0, messages.Length);
         
