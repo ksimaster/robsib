b4cad25df04a336c4eafb81ffc68402d2bcd0f2e
ksimaster <ksimasterlord@gmail.com>
2022-06-06 12:11:55 +0300

    PanelOnStart setting, create PauseSimple + fix

--- Assets/Scripts/StartPanel.cs
+++ Assets/Scripts/StartPanel.cs
@@ -4,23 +4,28 @@ using UnityEngine.UI;
 public class StartPanel : MonoBehaviour
 {
     public GameObject startPanel;
+    public GameObject panelOnStart;
     public Button button;
 
     private void Awake()
     {
         button.onClick.AddListener(() =>
         {
-            Time.timeScale = 1f;
+            Time.timeScale = 0f;
             PlayerPrefs.SetInt(PlayerConstants.IsEducationComplete, 1);
+            panelOnStart.SetActive(true); // запуск панели с предысторией
             startPanel.SetActive(false);
         });
 
         if (PlayerPrefs.GetInt(PlayerConstants.IsEducationComplete) == 0)
         {
             Time.timeScale = 0f;
+           
         }
         else
         {
+            panelOnStart.SetActive(true); // запуск панели с предысторией
+            Time.timeScale = 0f;
             startPanel.SetActive(false);
         }
     }
